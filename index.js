b79c7f37afc73ee6291e1f2a0a3238bbaad59363
almamutuguti <alma.wanjiru8j@gmail.com>
2025-08-25 10:58:43 +0300

    add mongoose and mongodb

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 const PORT = 3000;
 
 // having api in json
 app.use(express.json())
 
-let products = [
-    {
-        id: 1,
-        name: "Laptop",
-        price: 80000
-    },
+// let products = [
+//     {
+//         id: 1,
+//         name: "Laptop",
+//         price: 80000
+//     },
 
-    {
-        id: 2,
-        name: "phone",
-        price: 50000
-    }
+//     {
+//         id: 2,
+//         name: "phone",
+//         price: 50000
+//     }
 
-]
+// ]
+
+mongoose.connect()
 
 app.get("/api/products", (req, res) => {
     res.json(products)
 }) // created an end point to fetch the data
+
+app.post("/api/products", (req, res) => {
+    const { name, price } = req.body
+    const newProduct = {
+        id: products.length + 1,
+        name,
+        price
+    };
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is runing at http://localhost:${PORT}`)
