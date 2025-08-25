dc6250d7974e2e1103b26d7a944a2c0e91859bc2
almamutuguti <alma.wanjiru8j@gmail.com>
2025-08-25 11:27:28 +0300

    add mongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotevn from "dotenv"
+
+dotevn.config();
+
 const app = express();
-const PORT = 3000;
+
 
 // having api in json
 app.use(express.json())
@@ -21,22 +25,48 @@ app.use(express.json())
 
 // ]
 
-mongoose.connect()
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected successfully."))
+    .catch((err) => console.error("MongoDB connection error:", err))
 
-app.get("/api/products", (req, res) => {
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    description: {type: String, required: false},
+})
+
+const Product = mongoose.model("Product", productSchema)
+
+// app.get("/api/products", (req, res) => {
+//     res.json(products)
+// }) // created an end point to fetch the data
+app.get("/api/products", async (req, res) => {
+    const products = await Product.find();
     res.json(products)
-}) // created an end point to fetch the data
+}) 
 
-app.post("/api/products", (req, res) => {
-    const { name, price } = req.body
-    const newProduct = {
-        id: products.length + 1,
-        name,
-        price
-    };
-    products.push(newProduct);
-    res.status(201).json(newProduct);
+// app.post("/api/products", (req, res) => {
+//     const { name, price } = req.body
+//     const newProduct = {
+//         id: products.length + 1,
+//         name,
+//         price
+//     };
+//     products.push(newProduct);
+//     res.status(201).json(newProduct);
+// });
+app.post("/api/products", async (req, res) => {
+    try {
+        const product = new Product(req.body)
+        await product.save()
+        res.status(201).json(product)
+    } catch(err) {
+        res.status(400).json({ message: err.message })
+    }
 });
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server is runing at http://localhost:${PORT}`)
